3f4c263bf886728315b4a079fec7590b24c2ba0c
Stefan <you@example.com>
2016-04-19 14:16:56 -0400

    Refactored join for symbolic states

--- src/main/scala/org/phpex/statements/IfStatement.scala
+++ src/main/scala/org/phpex/statements/IfStatement.scala
@@ -5,19 +5,18 @@ import scala.collection.immutable.Stack
 import org.phpex.environments.Environment
 import org.phpex.environments.SimpleEnvironment
 import org.phpex.expressions.Expression
-import org.phpex.expressions.Variable
-import org.phpex.expressions.Variable
 import org.phpex.values.Value
 import org.phpex.values.concrete.StringValue
 import org.phpex.values.symbolic.Choice
+
 
 case class IfStatement(pass: Expression, s1: Statement, s2: Statement) extends Statement {
 
   def execute(env: Environment): Environment = {
     if (pass.evaluate(env).boolValue) {
-      return s1.execute(env.addOutput(null))
+      return s1.execute(env)
     } else {
-      return s2.execute(env.addOutput(null))
+      return s2.execute(env)
     }
   }
 
@@ -25,7 +24,7 @@ case class IfStatement(pass: Expression, s1: Statement, s2: Statement) extends S
    * addOutput(null) for marking purpose
    */
   def symbolicExecute(env: Environment): Environment = {
-    joinStates(env, s1.execute(env.addOutput(null)), s2.execute(env.addOutput(null)))// pop since null is added prior to that
+    joinStates(env, s1.execute(env), s2.execute(env)) // pop since null is added prior to that
   }
 
   private def joinStateMaps(m1: Map[String, Value], m2: Map[String, Value]): Map[String, Value] = {
@@ -52,57 +51,65 @@ case class IfStatement(pass: Expression, s1: Statement, s2: Statement) extends S
     }
     return smap
   }
-  
+
   /**
    * Recursively compares two stacks of Value objects.
    */
   private def compareStacks(s1: Stack[Value], s2: Stack[Value]): Boolean = {
-    if (s1.size != s2.size) {
+
+    var a = s1
+    var b = s2
+    if (a.isEmpty != b.isEmpty) {
       return false
     }
-    if (s1.isEmpty) {
-      true
-    } else {
+    if (a.isEmpty && b.isEmpty) {
+      return true
+    }
+    val element_a = a.top
+    a = a.pop
+    val element_b = b.top
+    b = b.pop
 
-      // TODO evtly implement a equals operator for Values
-      if (s1.top.toString.equals(s2.top.toString)) {
-        return compareStacks(s1.pop, s2.pop)
-      } else {
+    try {
+      if (((element_a == null) && (element_b != null)) || (!element_a.equals(element_b)))
         return false
-      }
+      return compareStacks(a, b)
+    } finally { // restore elements
+      a = a.push(element_a)
+      b = b.push(element_b)
     }
   }
 
-  private def joinStacks(p:Expression, s:Stack[Value], s1: Stack[Value], s2: Stack[Value]): Stack[Value] = {
+  private def joinStacks(p: Expression, s1: Stack[Value], s2: Stack[Value]): Stack[Value] = {
 
-    var s1c = s1
-    var output1 = new Stack[Value]
-    while (s1c.top != null) {
-      output1 = output1.push(s1c.top)
-      s1c = s1c.pop
-    }
-
-    var s2c = s2
-    var output2 = new Stack[Value]
-    while (s2c.top != null) {
-      output2 = output2.push(s2c.top)
-      s2c = s2c.pop
-    }
-
+    val prefix = pref(s1.reverse, s2.reverse)  
+    val output1 = s1.reverse.slice(prefix.size, s1.size)
+    val output2 = s2.reverse.slice(prefix.size, s2.size)  
+    
+    System.err.println(prefix)
+    
     if (compareStacks(output1, output2)) {
-      return s.pop ++ output1 // pop since null is added prior to that
+      return prefix ++ output1 // pop since null is added prior to that
     } else {
-      var stv1 = new StringValue(output1.foldLeft("")((a,b) => a + b))
-      var stv2 = new StringValue(output2.foldLeft("")((a,b) => a + b))
-      return s.pop.push( new Choice(p, stv1, stv2) ) // pop since null is added prior to that
+      var stv1 = new StringValue(output1.foldLeft("")((a, b) => a + b))
+      var stv2 = new StringValue(output2.foldLeft("")((a, b) => a + b))
+      return prefix.push(new Choice(p, stv1, stv2)).reverse // pop since null is added prior to that
     }
 
   }
 
-  def joinStates(env:Environment, env1: Environment, env2: Environment): Environment = {
+  def joinStates(env: Environment, env1: Environment, env2: Environment): Environment = {
     var joinedMap = joinStateMaps(env1.getMap(), env2.getMap())
-    var joinedOutput = joinStacks(pass, env.getOutput(), env1.getOutput(), env2.getOutput())
+    var joinedOutput = joinStacks(pass, env1.getOutput(), env2.getOutput())
     return new SimpleEnvironment(joinedMap, joinedOutput)
+  }
+  
+  private def pref(s: Stack[Value], t: Stack[Value], out: Stack[Value] = Stack[Value]()): Stack[Value] = {
+    if (s.isEmpty || t.isEmpty || !s(0).equals(t(0))) {
+      return out
+    } else {
+      pref(s.slice(1, s.size), t.slice(1, t.size), out.push(s(0)))
+    }
   }
 
 }
