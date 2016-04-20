c8ee9b298ab8674f358ee2748ca8b9133235e853
Stefan <you@example.com>
2016-04-20 15:12:24 -0400

    added return statement detection

--- src/main/scala/org/phpex/statements/BlockStatement.scala
+++ src/main/scala/org/phpex/statements/BlockStatement.scala
@@ -6,13 +6,27 @@ case class BlockStatement(statements:List[Statement]) extends Statement {
   
   def execute(env:Environment): Environment = {
     var environment = env
-    statements.foreach { stmt => environment = stmt.execute(environment) }
+    statements.foreach { stmt => 
+      environment = stmt.execute(environment) 
+      
+      if (stmt.isInstanceOf[ReturnStatement]) {
+        return environment
+      }
+    }
     return environment
   }
   
   def symbolicExecute(env:Environment): Environment = {
     var environment = env
-    statements.foreach { stmt => environment = stmt.symbolicExecute(environment) }
+    statements.foreach { stmt => 
+      environment = stmt.symbolicExecute(environment) 
+      
+      if (stmt.isInstanceOf[ReturnStatement]) {
+        return environment
+      }
+    }
     return environment
   }
+  
+  override def toString() = statements mkString "\n"
 }
