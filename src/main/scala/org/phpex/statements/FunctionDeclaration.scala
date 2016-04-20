a8d7d2d367a392eb6716c555db522f7ac7f5c88d
Stefan <you@example.com>
2016-04-20 15:19:31 -0400

    added function declaration statement

---
+++ src/main/scala/org/phpex/statements/FunctionDeclaration.scala
@@ -0,0 +1,17 @@
+package org.phpex.statements
+
+import org.phpex.environments.Environment
+import org.phpex.values.concrete.FunctionValue
+import com.sun.org.apache.xalan.internal.xsltc.compiler.ForEach
+
+case class FunctionDeclaration(name:String, args:List[String], body:BlockStatement) extends Statement {
+  
+  def execute(env:Environment): Environment = {
+    return env.update(name, FunctionValue(args, body))
+  }
+  
+  def symbolicExecute(env:Environment): Environment = ???
+  
+  override def toString() = "define " + name + "(" + args.foreach { arg => arg } + ") {" + body + "}"
+  
+}
