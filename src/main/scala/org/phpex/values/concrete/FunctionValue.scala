da2a3e059be05c851c484ccfcff0e41dd6cb0946
Stefan <you@example.com>
2016-04-20 15:12:58 -0400

    added function value class

---
+++ src/main/scala/org/phpex/values/concrete/FunctionValue.scala
@@ -0,0 +1,14 @@
+package org.phpex.values.concrete
+
+import org.phpex.values.Value
+import org.phpex.statements.Statement
+import org.phpex.statements.BlockStatement
+
+case class FunctionValue(args:List[String], body:BlockStatement) extends Value {
+  
+  def getArgs() = args
+  
+  def intValue = ???
+  def boolValue = ???
+  override def toString = "FUNCTION(" + (args mkString ",") + ") {\n" + body.toString() + "\n}"
+}
