7a5a10370b282d1b0e8792c15e4f2769d99f2ad2
Stefan <you@example.com>
2016-04-20 01:25:10 -0400

    refactoring

--- src/main/scala/org/phpex/expressions/bool/And.scala
+++ src/main/scala/org/phpex/expressions/bool/And.scala
@@ -4,6 +4,7 @@ import org.phpex.expressions.BooleanExpression
 import org.phpex.environments.Environment
 import org.phpex.values.concrete.BooleanValue
 import org.phpex.expressions.Expression
+import org.phpex.expressions.Expression
 
 case class And(b1:Expression, b2:Expression) extends BooleanExpression {
   def evaluate(env:Environment) = BooleanValue(b1.evaluate(env).boolValue && b2.evaluate(env).boolValue)
