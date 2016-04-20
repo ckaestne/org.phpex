c2ddbaca8d953f67dc697635fbf96924cf70e7c5
Stefan <you@example.com>
2016-04-20 15:12:00 -0400

    serialization updated/altered

--- src/main/scala/org/phpex/expressions/integer/Add.scala
+++ src/main/scala/org/phpex/expressions/integer/Add.scala
@@ -7,5 +7,5 @@ import org.phpex.environments.Environment
 
 case class Add(i1:Expression, i2:Expression) extends IntegerExpression {
   def evaluate(env:Environment) = IntegerValue(i1.evaluate(env).intValue + i2.evaluate(env).intValue)
-  override def toString() = "Add("+i1+", "+i2+")"
+  override def toString() = "(" + i1 +" + " + i2 + ")"
 }
--- src/main/scala/org/phpex/expressions/integer/Mod.scala
+++ src/main/scala/org/phpex/expressions/integer/Mod.scala
@@ -7,4 +7,5 @@ import org.phpex.expressions.Expression
 
 case class Mod(i1:Expression, i2:Expression) extends IntegerExpression {
   def evaluate(env:Environment) = IntegerValue(i1.evaluate(env).intValue % i2.evaluate(env).intValue)
+  override def toString() = "(" + i1 +" % " + i2 + ")"
 }
