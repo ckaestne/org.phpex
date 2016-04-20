06b1b805f652d1066cf3c227e5c879598e0ddcfb
Stefan <you@example.com>
2016-04-20 15:09:57 -0400

    added test classes for functions & function declarations

---
+++ src/test/scala/org/phpex/statements/FunctionDeclarationTest.scala
@@ -0,0 +1,52 @@
+package org.phpex.statements
+
+import org.phpex.expressions.Variable
+import org.phpex.expressions.Variable
+import org.phpex.expressions.integer.Mul
+import org.scalatest.FlatSpec
+import org.phpex.environments.SimpleEnvironment
+import org.phpex.values.concrete.FunctionValue
+import org.phpex.statements.FunctionDeclaration
+import org.phpex.expressions.Variable
+import org.phpex.statements.IfStatement
+import org.phpex.expressions.bool.GreaterThan
+import org.phpex.expressions.integer.IntegerConstant
+import org.phpex.expressions.integer.Add
+import org.phpex.expressions.Call
+
+object FunctionDeclarationTest {
+  
+  /**
+   * function f(x, y) {
+   * 	return x * y;
+   * }
+   */
+  def functionDeclaration1(): Statement = FunctionDeclaration("f", List("x", "y"), BlockStatement(List(ReturnStatement(Mul(Variable("x"), Variable("y"))))))
+   
+  /**
+   * function f(x) {
+   * 	if (x > 0) {
+   * 		return f(x - 1);
+   *   else {
+   *   	return x;
+   *   }
+   * }
+   */
+  def recursiveExample1(): Statement = FunctionDeclaration("f", List("x"), 
+      BlockStatement(List(
+          IfStatement(GreaterThan(Variable("x"), IntegerConstant(0)),
+              BlockStatement(List(ReturnStatement(Call("f", List(Add(Variable("x"), IntegerConstant(-1))))))),
+              BlockStatement(List(ReturnStatement(Variable("x"))))))))
+              
+              
+
+}
+
+class FunctionDeclarationTest extends FlatSpec {
+  
+  "Function declaration" should "assign a function value to f" in {
+    val f = FunctionValue(List("x", "y"), BlockStatement(List(ReturnStatement(Mul(Variable("x"), Variable("y"))))))
+    assert( FunctionDeclarationTest.functionDeclaration1().execute(SimpleEnvironment()).lookup("f").equals(f) )
+  }
+  
+}
---
+++ src/test/scala/org/phpex/statements/ReturnStatementTest.scala
@@ -0,0 +1,57 @@
+package org.phpex.statements
+
+import org.scalatest.FlatSpec
+import org.phpex.environments.SimpleEnvironment
+import org.phpex.expressions.integer.IntegerConstant
+import org.phpex.expressions.Call
+import org.phpex.values.concrete.IntegerValue
+import org.phpex.values.concrete.IntegerValue
+import org.phpex.expressions.integer.IntegerConstant
+
+object ReturnStatementTest {
+  
+  /**
+   * function f(x, y) {
+   * 	return x * y;
+   * }
+   * a = f(2,3);
+   */
+  def returnStatementExample1(): Statement = BlockStatement(List(
+      FunctionDeclarationTest.functionDeclaration1(), 
+      AssignStatement("a", Call("f", List(IntegerConstant(2), IntegerConstant(32))))))
+      
+  /**
+   * function f(x) {
+   * 	if (x > 0) {
+   * 		return f(x - 1);
+   *   else {
+   *   	return x;
+   *   }
+   * }
+   * a = f(10)
+   */
+  def returnStatementExample2(): Statement = BlockStatement(List(
+      FunctionDeclarationTest.recursiveExample1(), 
+      AssignStatement("a", Call("f", List(IntegerConstant(10))))))
+
+}
+
+class ReturnStatementTest extends FlatSpec {
+  
+  "Function with return statement simply executed" should "return a correct value" in {
+    assert(ReturnStatementTest.returnStatementExample1().execute(SimpleEnvironment()).lookup("a").equals(IntegerValue(64)))
+  }
+  
+  "Function with return statement symbolically executed" should "return a correct value" in {
+    assert(false)
+  }
+  
+  "Recursive function with return statement simply executed" should "return a correct value" in {
+    assert(ReturnStatementTest.returnStatementExample2().execute(SimpleEnvironment()).lookup("a").equals(IntegerValue(0)))
+  }
+  
+  "Recursive function with return statement symbolically executed" should "detect recursion" in {
+    assert(false)
+  }
+  
+}
