9428d613f70bb30e9478246b82bfa4cfcfb720f2
Stefan <you@example.com>
2016-04-22 13:37:32 -0400

    implemented evaluation method

--- src/main/scala/org/phpex/values/symbolic/Choice.scala
+++ src/main/scala/org/phpex/values/symbolic/Choice.scala
@@ -3,11 +3,25 @@ package org.phpex.values.symbolic
 import org.phpex.values.SymbolicValue
 import org.phpex.values.Value
 import org.phpex.expressions.Expression
+import org.phpex.environments.Environment
+import org.phpex.values.concrete.BooleanValue
 
 case class Choice(p:Expression, v1:Value, v2:Value) extends SymbolicValue {
   def intValue: Integer = 0
   def boolValue: Boolean = false
-  //def stringValue: String
+  
+  def evaluate(env:Environment): Value = {
+    val pv = p.evaluate(env)
+    if (pv.isInstanceOf[BooleanValue]) {
+      if (pv.asInstanceOf[BooleanValue].equals(true)) {
+        return v1
+      } else {
+        return v2
+      }
+    } else {
+      return null
+    }
+  }
   
   override def toString() = "CHOICE(" + p.toString + ", " + v1 + ", " + v2 + ")"
 }
--- src/main/scala/org/phpex/values/symbolic/SymbolValue.scala
+++ src/main/scala/org/phpex/values/symbolic/SymbolValue.scala
@@ -1,7 +1,11 @@
 package org.phpex.values.symbolic
 
 import org.phpex.values.SymbolicValue
+import org.phpex.values.Value
+import org.phpex.environments.Environment
 
 case class SymbolValue(name:String) extends SymbolicValue {
+  
+  def evaluate(env:Environment): Value = this
   
 }
